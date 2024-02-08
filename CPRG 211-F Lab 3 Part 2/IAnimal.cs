2069110ea7714c3581b3757f286ace4cb75f466d
BrienMC <152676467+BrienMC@users.noreply.github.com>
2024-02-07 20:33:33 -0700

    Add files via upload

---
+++ CPRG 211-F Lab 3 Part 2/IAnimal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPRG_211_F_Lab_3_Part_2
+{
+    public interface IAnimal
+    {
+        string? Name { get; set; }
+        string? Colour { get; set; }
+        int Height { get; set; }
+        int Age { get; set; }
+
+        public abstract void Eat();
+        public string Cry();
+
+    }
+
+    public class Dog : IAnimal
+    {
+        public string? Name { get; set; }
+        public string? Colour { get; set; }
+        public int Height { get; set; }
+        public int Age { get; set; }
+
+        public virtual void Eat()
+        {
+            Console.WriteLine("Dogs eat meat");
+        }
+        
+        public virtual string Cry()
+        {
+            return "Woof!";
+        }
+
+    }
+
+    public class Cat : IAnimal
+    {
+        public string? Name { get; set; }
+        public string? Colour { get; set; }
+        public int Height { get; set; }
+        public int Age { get; set; }
+
+        public virtual void Eat()
+        {
+            Console.WriteLine("Cats eat mice");
+        }
+
+        public virtual string Cry()
+        {
+            return "Meow!";
+        }
+
+    }
+}
---
+++ CPRG 211-F Lab 3 Part 2/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace CPRG_211_F_Lab_3_Part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IAnimal dog = new Dog();
+            IAnimal cat = new Cat();
+
+
+            //Dog Class inherited from IAnimal
+
+            Console.WriteLine("Enter dog name: ");
+
+            dog.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter dog color: ");
+
+            dog.Colour = Console.ReadLine();
+
+            Console.WriteLine("Enter dog height: ");
+
+            dog.Height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter dog age: ");
+
+            dog.Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nThe dog's name is {dog.Name}, its colour is {dog.Colour}, is {dog.Height}cm tall, and is {dog.Age} years old.");
+            dog.Eat();
+            Console.WriteLine(dog.Cry());
+            Console.WriteLine();
+
+
+
+            //Cat Class inherited from IAnimal
+
+            Console.WriteLine("Enter cat name: ");
+
+            cat.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter cat color: ");
+
+            cat.Colour = Console.ReadLine();
+
+            Console.WriteLine("Enter cat height: ");
+
+            cat.Height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter cat age: ");
+
+            cat.Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nThe cat's name is {cat.Name}, its colour is {cat.Colour}, is {cat.Height}cm tall, and is {cat.Age} years old.");
+            cat.Eat();
+            Console.WriteLine(cat.Cry());
+            Console.WriteLine();
+
+
+
+            // List of animals and prints out all of their names
+
+            IAnimal dog2 = new Dog();
+            dog2.Name = "Max";
+            IAnimal cat2 = new Cat();
+            cat2.Name = "Luna";
+            IAnimal dog3 = new Dog();
+            dog3.Name = "Buster";
+            IAnimal cat3 = new Cat();
+            cat3.Name = "Milo";
+
+
+            List<IAnimal> animals = new List<IAnimal>();
+            animals.Add(dog);
+            animals.Add(cat);
+            animals.Add(dog2);
+            animals.Add(cat2);
+            animals.Add(dog3);
+            animals.Add(cat3);
+
+            foreach (IAnimal animal in animals) {
+
+                Console.WriteLine(animal.Name);
+
+            }
+
+        }
+    }
+}
